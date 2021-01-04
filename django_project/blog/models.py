ea408860bf94b4da985e2ab71a7a3a164cf1ec57
moaaz-ashour <ashour.m@live.de>
2021-01-04 15:04:57 +0100

    return human readable string

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -7,3 +7,6 @@ class Post(models.Model):
     data_posted = models.DateTimeField(default=timezone.now)
     # author = models.ForeignKey(User,on_delete=models.CASCADE) if user deleted, their post will also be deleted
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.title
