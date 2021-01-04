ca5820ba481611d94377a96e7569ec65b9fbd924
moaaz-ashour <ashour.m@live.de>
2021-01-04 16:06:18 +0100

    register Post model

--- django_project/blog/admin.py
+++ django_project/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'date_posted', 'author')  # display fields
+    list_display_links = ('title', 'date_posted', 'author') # make fields clickable
+    search_fields = ['date_posted', 'author'] # search by
+    list_filter = ('author',) # filter
+    autocomplete_fields = ['author']
