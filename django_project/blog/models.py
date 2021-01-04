94ebf07034d8bddda9b7ba501ba333873405abf3
moaaz-ashour <ashour.m@live.de>
2021-01-04 14:55:03 +0100

    post <> user model - many-to-many

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
-# Create your models here.
+from django.utils import timezone
+from django.contrib.auth.models import User
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    data_posted = models.DateTimeField(default=timezone.now)
+    # author = models.ForeignKey(User,on_delete=models.CASCADE) if user deleted, their post will also be deleted
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
