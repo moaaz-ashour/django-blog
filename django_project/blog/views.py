d50629ac1c048a3cedf65ddc9687afcf4ab9888c
moaaz-ashour <ashour.m@live.de>
2021-01-04 15:28:36 +0100

    query data from db

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Post
+
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
