cadd049862842051ee2ce9ed75900011e52b1865
moaaz-ashour <ashour.m@live.de>
2021-01-04 15:36:24 +0100

    fix typo

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    data_posted = models.DateTimeField(default=timezone.now)
+    date_posted = models.DateTimeField(default=timezone.now)
     # author = models.ForeignKey(User,on_delete=models.CASCADE) if user deleted, their post will also be deleted
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
