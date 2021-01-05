582b39c5661669637aac87d401dbce47f17a9b92
moaaz-ashour <ashour.m@live.de>
2021-01-05 11:50:32 +0100

    adding a message if form is valid

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
@@ -8,6 +9,7 @@ def register(request):
         # validate the form when submitted
         if form.is_valid():
             username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
             return redirect('blog-home')
     else:
         form = UserCreationForm()
