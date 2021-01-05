28730fefdeb35c76c01c5e652be0d2697f58f266
moaaz-ashour <ashour.m@live.de>
2021-01-05 12:06:44 +0100

    rendering new form instead of UserCreationForm

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+
+from .forms import UserRegisterForm
 
 def register(request):
     if request.method == 'POST':
         # instantiate form with post data
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         # validate the form when submitted
         if form.is_valid():
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}')
             return redirect('blog-home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
