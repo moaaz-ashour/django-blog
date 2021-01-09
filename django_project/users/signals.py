2009492e94ff7719bc7186fcaf1c058af4f7c1cb
Moaaz Ashour <ashour.m@live.de>
2021-01-09 22:01:46 +0100

    signal 1 - auto Profile creation for new User

---
+++ django_project/users/signals.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import Profile
+
+"""
+    Signal: post_save(): gets fired after User is saved..
+    Signal Sender: User
+    Signal Receiver: @decorator(signal, sender) > create_profile
+
+    create_profile: a function which runs everytime a User is created decorated with @receiver
+"""
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
