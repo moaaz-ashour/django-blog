bf9c1c5c7e2c2f205f9894685edf49c911a80300
Moaaz Ashour <ashour.m@live.de>
2021-01-09 21:42:40 +0100

    serve files uploaded by user - dev env

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from django.contrib.auth import views as auth_views # class_based build_in views
+from django.contrib.auth import views as auth_views  # class_based build_in views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.urls import path, include
 from users import views as user_views
 
@@ -11,3 +15,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),    
     path('', include('blog.urls')),
 ]
+
+# Django docs:
+if settings.DEBUG: # be more specific
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
