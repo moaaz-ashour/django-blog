0882869b977814b45a4dca9fceb774e655f9aa95
Moaaz Ashour <ashour.m@live.de>
2021-01-10 15:56:32 +0100

    add comment

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -10,6 +10,8 @@ def home(request):
     return render(request, 'blog/home.html', context)
 
 class PostListView(ListView):
+    # which model to query to create the List View?
+    # by convention, this is all what you need to get going..
     model = Post
 
     #2. by default, Django is gonna look for <app>/<model>_<viewtype>.html which is out case should be blog/post_list.html .. since we have another template for that, we can tell Django:
@@ -17,6 +19,9 @@ class PostListView(ListView):
 
     #3. by default, out list view is going to call variable object list. So either go to template and loop through object list, or we can set one more variable in our list view and let the class know that we want that variable to be called "post" instead.
     context_object_name = 'posts'
+
+    #4. change order of displayed posts
+    ordering = ['-date_posted']
 
 def about(request):
     return render(request, 'blog/about.html')
