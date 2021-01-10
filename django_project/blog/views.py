c714ef86e7b3743011d1bc9764a0d9c9fc79f42b
Moaaz Ashour <ashour.m@live.de>
2021-01-10 15:52:29 +0100

    create new variable to loop over in template instead of django's default

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -15,6 +15,8 @@ class PostListView(ListView):
     #2. by default, Django is gonna look for <app>/<model>_<viewtype>.html which is out case should be blog/post_list.html .. since we have another template for that, we can tell Django:
     template_name = 'blog/home.html'
 
+    #3. by default, out list view is going to call variable object list. So either go to template and loop through object list, or we can set one more variable in our list view and let the class know that we want that variable to be called "post" instead.
+    context_object_name = 'posts'
 
 def about(request):
     return render(request, 'blog/about.html')
