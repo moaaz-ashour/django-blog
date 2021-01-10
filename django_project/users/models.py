aa5a191187640ffb7ea30d7f1a1200c343ca6b77
Moaaz Ashour <ashour.m@live.de>
2021-01-10 11:38:23 +0100

    rescale profile image with Pillow by overriding save() method

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
-        
+    
+    #override save() method
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
+        # get profile image of current instance
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            # define output img size
+            output_size = (300, 300)
+            # resize
+            img.thumbnail(output_size)
+            # save back to same path
+            img.save(self.image.path)
