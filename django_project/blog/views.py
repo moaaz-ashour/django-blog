2bcd832885a4e290f03557e50b6f2cb7217ef0c4
Moaaz Ashour <ashour.m@live.de>
2021-01-10 15:50:37 +0100

    define model to query to create the ListView

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -10,8 +10,7 @@ def home(request):
     return render(request, 'blog/home.html', context)
 
 class PostListView(ListView):
-    pass
-
+    model = Post
 
 def about(request):
     return render(request, 'blog/about.html')
