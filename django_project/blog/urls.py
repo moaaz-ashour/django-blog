444303e17560840672f6ab7fd33071e51fd475d7
Moaaz Ashour <ashour.m@live.de>
2021-01-11 20:34:41 +0100

    URL pattern for User posts

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -4,12 +4,14 @@ from .views import (
     PostDetailView,
     PostCreatelView,
     PostUpdatelView,
-    PostDeleteView
+    PostDeleteView,
+    UserPostsListView
 )
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostsListView.as_view(), name='user-post'),
     path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
     path('post/new', PostCreatelView.as_view(), name='post-create'),
     path('post/<int:pk>/update', PostUpdatelView.as_view(), name='post-update'),
