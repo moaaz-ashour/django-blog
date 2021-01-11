e378af4cfa1bde96b22f9ca34999af222237d8f3
Moaaz Ashour <ashour.m@live.de>
2021-01-11 20:42:05 +0100

    fix typo

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -11,7 +11,7 @@ from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
-    path('user/<str:username>', UserPostsListView.as_view(), name='user-post'),
+    path('user/<str:username>', UserPostsListView.as_view(), name='user-posts'),
     path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
     path('post/new', PostCreatelView.as_view(), name='post-create'),
     path('post/<int:pk>/update', PostUpdatelView.as_view(), name='post-update'),
