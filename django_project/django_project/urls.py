bfaa4b41517c5661f545ad83ce9106210e34c663
Moaaz Ashour <ashour.m@live.de>
2021-01-12 12:21:57 +0100

    password_rest URL pattern

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth import views as auth_views  # class_based build_in views
+from django.contrib.auth import views as auth_views 
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -13,6 +13,9 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),    
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),    
+    path('password-reset/', 
+        auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), 
+        name='password_reset'),    
     path('', include('blog.urls')),
 ]
 
