996d3b68a37806245d4ae4c077546d14d0ed6808
Moaaz Ashour <ashour.m@live.de>
2021-01-12 23:45:45 +0100

    no need to resize since using S3

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -7,16 +7,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
-    
     #override save() method
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        # get profile image of current instance
-        img = Image.open(self.image.path)
-        if img.height > 300 or img.width > 300:
-            # define output img size
-            output_size = (300, 300)
-            # resize
-            img.thumbnail(output_size)
-            # save back to same path
-            img.save(self.image.path)
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
+    #     # get profile image of current instance
+    #     img = Image.open(self.image.path)
+    #     if img.height > 300 or img.width > 300:
+    #         # define output img size
+    #         output_size = (300, 300)
+    #         # resize
+    #         img.thumbnail(output_size)
+    #         # save back to same path
+    #         img.save(self.image.path)
