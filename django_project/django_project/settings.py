bea757480bd141cdcd88688f3afe118933d24b53
Moaaz Ashour <ashour.m@live.de>
2021-01-12 14:24:35 +0100

    modify configurations to send emails through SMTP server

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -143,4 +143,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = reverse_lazy('login')
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = str(os.getenv('EMAIL_USER'))
+EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_PASSWORD'))
