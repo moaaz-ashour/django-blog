c255d0f48ffbf5b62fa700c38308cdafda8ba238
Moaaz Ashour <ashour.m@live.de>
2021-01-17 18:56:34 +0100

    restructuring settings.py Adding notes to settings.py

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -15,6 +15,7 @@ import django_on_heroku
 from pathlib import Path
 from django.urls import reverse_lazy
 from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,17 +25,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-load_dotenv()
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (str(os.getenv('DEBUG_VALUE')) == 'True')
+DEBUG = (str(os.getenv('DEBUG_VALUE')) == 'True') # value as bool.
 
 ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -87,7 +86,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'django_project.wsgi.application'
 
 
-# Database
+# Database ( for development )
+# For deployment, we use Postgres. 
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
@@ -121,13 +121,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
@@ -141,17 +137,21 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # for static files (Heroku)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # full Path to where you want Django to save uploaded files.
 MEDIA_URL = '/media/'  # public URL of MEDIA_ROOT directory
 
+# for django-crispy-forms=
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+# Login URL and redirection
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = reverse_lazy('login')
 
+# SMTP Configs:
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = str(os.getenv('EMAIL_USER'))
 EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_PASSWORD'))
+
 
 # Django Storages settings to upload media files to S3.
 # S3 BUCKET CONFIG
