a70596cdf2ca2194872eced622ad22b89822fd66
Moaaz Ashour <ashour.m@live.de>
2021-01-17 17:03:17 +0100

    prepare for deployment- install django_on_heroku package

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
+import django_on_heroku
 from pathlib import Path
 from django.urls import reverse_lazy
-import os
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
